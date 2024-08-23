65a697d347aba197064457a547f00be8c37a224c
bastelpingu <michael_vogel91@gmx.ch>
2024-08-23 20:29:53 +0200

    Update DriftBlitz.ino

--- DriftBlitz.ino
+++ DriftBlitz.ino
@@ -4,4 +4,4 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();  // Objekt für Lasersensor
 #define LED_PIN D5  // LED auf PIN D5
 
 int distanz;  // Variable für die Distanz
-int distanzBlitz = 70;  // Variable für die Blitzauslösung
+int distanzBlitz = 70;  // Schwellenwert (in Millimetern) für die Blitzauslösung: Wenn die gemessene Distanz kleiner oder gleich diesem Wert ist, wird die LED ausgelöst
